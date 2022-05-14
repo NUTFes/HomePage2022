3842381a5b37c75c7318c48b4670b15e99c930d0
TsubasaOura <TsubasaOura@users.noreply.github.com>
2022-05-14 14:58:18 +0000

    deploy: be21469274458c5b816dd41fc57efd7a8b309782

---
+++ _next/static/pm85mrjZU2xOpSxuNBesy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ef97d8a4459c85ff.css","static/chunks/pages/index-953a8ba0bdb1b104.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
