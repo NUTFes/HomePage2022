9c27a182f57341e1a6ad60aa0d8a388a80ab897a
ryuseifujisaki <fujiryu1122@gmail.com>
2022-08-18 00:37:23 +0900

    [dev/issue58] 少し修正

--- src/pages/participation_group_infor.tsx
+++ src/pages/participation_group_infor.tsx
@@ -4,6 +4,7 @@ import Footer from "../components/common/Footer";
 import Sns from "../components/common/Sns";
 import Group_title from "../components/common/Group_title ";
 import Group_card from "../components/common/Group_card";
+import Group_courus from "../components/common/Group_courus";
 
 const Home: NextPage = () => {
   return (
@@ -19,7 +20,7 @@ const Home: NextPage = () => {
         />
       </Head>
       <Group_title />
-      <Group_card />
+      <Group_courus/>
       <Sns />
       <Footer />
     </>
