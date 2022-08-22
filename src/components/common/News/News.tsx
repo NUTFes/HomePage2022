19530c027ae8e78d42e8d696e019cc551d30fc0c
ryuseifujisaki <fujiryu1122@gmail.com>
2022-08-22 23:52:28 +0900

    [dev/issue70] 8/22日修正

--- src/components/common/News/News.tsx
+++ src/components/common/News/News.tsx
@@ -15,8 +15,8 @@ const News: FC =() => {
     <div className={s.topspace}></div>
       <div className={s.topspace}></div>
       <div className={s.minicard}>
-        <p className={s.middlefont} >・8月22日<br/>
-        <a href={process.env.URL+'/president_message'} className={s.link}>代表挨拶ページを更新しました</a></p>
+        <p className={s.middlefont} >8月22日<br/>
+        <a href={process.env.URL+'/president_message'} className={s.link}>・代表挨拶ページを更新しました</a></p>
         <p className={s.middlefont}>7月16日<br/>・ホームページを更新しました</p>
         </div>
     </div>
